c0a8c5e2f061d967784c76d48d6b594ac5e5fb3b
gibbonsr9 <54359236+gibbonsr9@users.noreply.github.com>
2019-11-15 09:59:34 -0500

    Update LGBTDetroit.tsx
    added link to involvement google form

--- views/lgbtdetroit/LGBTDetroit.tsx
+++ views/lgbtdetroit/LGBTDetroit.tsx
@@ -8,7 +8,11 @@ LGBT Detroit is a Detroit based 501c(3) nonprofit organization (EIN 56-2393981)
 whose mission is to increase awareness of and support to Detroit's dynamic LGBT
 culture through education and advocacy with integrity and pride.
 
-For more information, visit [our website](https://www.lgbtdetroit.org/).`;
+For more information, visit [our website](https://www.lgbtdetroit.org/).
+
+## Get Involved
+Want to get involved? [Fill out this form](https://docs.google.com/forms/d/1jYyERwDlWqVNupBTlddqHe3k6NAoCjB1O-COhIUPdyc/viewform?ts=596fc381&edit_requested=true).
+`;
 
 export const LGBTDetroitView: NarwhalRoute = () => {
   return <MarkdownView markdown={COPY} />;
