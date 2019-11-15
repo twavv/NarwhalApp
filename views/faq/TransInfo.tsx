e51064170d98d3a7da7c378a5bcd4b8126197c6a
Emmett Leddin <52934003+emleddin@users.noreply.github.com>
2019-11-15 10:27:12 -0500

    Missed 2 MD files

--- views/faq/TransInfo.tsx
+++ views/faq/TransInfo.tsx
@@ -4,20 +4,13 @@ import * as React from "react";
 
 const COPY = `# Information for Transgender Voters
 
-We understand that having legal documentation that does not accurately reflect
-personal identities can present certain challenges for transgender voters.
+We understand that having legal documentation that does not accurately reflect personal identities can present certain challenges for transgender voters.
 
-Michigan requires voters to show a form photo identification at the polls.
-Michigonians without a valid photo ID *can* still vote, after signing an
-affidavit attesting that they do not possess picture
-identification.
+Michigan requires voters to show a form photo identification at the polls. Michigonians without a valid photo ID *can* still vote, after signing an affidavit attesting that they do not possess picture identification.
 
-For more information, check out the National Center For Transgender Equality's
-[\#VotingWhileTrans Guide](https://transformthevote.org/s/voting-while-trans-c4-en.pdf).
+For more information, check out the National Center For Transgender Equality's [\#VotingWhileTrans Guide](https://transformthevote.org/s/voting-while-trans-c4-en.pdf).
 
-Para obtener información, mire la
-[Guía de \#VotingWhileTrans](https://transformthevote.org/s/voting-while-trans-c4-es-final.pdf)
-de National Center For Transgender Equality.`;
+Para obtener información, mire la [Guía de \#VotingWhileTrans](https://transformthevote.org/s/voting-while-trans-c4-es-final.pdf) de National Center For Transgender Equality.`;
 
 export const FAQTransInfoView: NarwhalRoute = () => {
   return <MarkdownView markdown={COPY} />;
--- views/faq/VoterID.tsx
+++ views/faq/VoterID.tsx
@@ -4,16 +4,13 @@ import * as React from "react";
 
 const COPY = `# Voter ID Requirement
 
-Michigan requires voters to show a form photo identification at the polls.
-Michigonians without a valid photo ID **can** still vote, after signing an
-affidavit attesting that they do not possess picture identification.
+Michigan requires voters to show a form photo identification at the polls. Michigonians without a valid photo ID **can** still vote, after signing an affidavit attesting that they do not possess picture identification.
 
 **Note**: IDs do not need your address.
 
 ## Valid Identification
 
-The following identity documents can be used to satisfy the ID requirement
-if they are current:
+The following identity documents can be used to satisfy the ID requirement if they are current:
 
 - Michigan driver’s license
 - Michigan personal identification card
@@ -21,13 +18,11 @@ if they are current:
 - Federal or state government-issued photo ID
 - U.S. passport
 - Military identification card with photo
-- Student identification with photo from a high school or an
-accredited institution of higher education
+- Student identification with photo from a high school or an accredited institution of higher education
 - Tribal identification card with photo
 
 ## Voting without ID
-Voters who do not have acceptable picture ID or forgot to bring acceptable
-picture ID to the polls can vote like any other voter by signing an affidavit.
+Voters who do not have acceptable picture ID or forgot to bring acceptable picture ID to the polls can vote like any other voter by signing an affidavit.
 
 More information can be found [here](https://www.michigan.gov/sos/0,4670,7-127-1633_8716-178123--,00.html).`;
 
