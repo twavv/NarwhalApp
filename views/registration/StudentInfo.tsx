77ff382a90cd64c7386c37e311e00a6834aad800
emleddin <52934003+emleddin@users.noreply.github.com>
2019-11-15 08:30:38 -0500

    Create StudentInfo

---
+++ views/registration/StudentInfo.tsx
@@ -0,0 +1,7 @@
+export default `# Student Voters
+
+Students get to choose where they register to vote. College students may register at their 
+campus address or register (or remain registered) at their permanent or home address.
+Some students may find it is easier to register locally than vote absentee through 
+their home/permanent address.
+**Important**: You may only be registered and vote in one location.`;
