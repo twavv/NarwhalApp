53d93843f83b3e4abc544ea3f6aad8b995f2d5ea
emleddin <52934003+emleddin@users.noreply.github.com>
2019-11-15 08:31:30 -0500

    Add student info source

--- views/registration/StudentInfo.tsx
+++ views/registration/StudentInfo.tsx
@@ -4,4 +4,7 @@ Students get to choose where they register to vote. College students may registe
 campus address or register (or remain registered) at their permanent or home address.
 Some students may find it is easier to register locally than vote absentee through 
 their home/permanent address.
-**Important**: You may only be registered and vote in one location.`;
+**Important**: You may only be registered and vote in one location.
+
+
+[Source: campusvoteproject.org](https://www.campusvoteproject.org/stateguides/Michigan)`;
